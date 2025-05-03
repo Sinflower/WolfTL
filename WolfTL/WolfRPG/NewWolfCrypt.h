80291ebd32989ac58eaf2e57e4041ebfdd2320e8
Sinflower <s1nflower@yandex.com>
2025-05-04 01:58:50 +0200

    Updated NewWolfCrypt.h to reflect the latest changes

--- WolfTL/WolfRPG/NewWolfCrypt.h
+++ WolfTL/WolfRPG/NewWolfCrypt.h
@@ -35,14 +35,28 @@
 #include <string>
 #include <vector>
 
-static constexpr uint8_t sbox[256] = {
+
+inline uint32_t xorshift32(const uint32_t &seed = 0)
+{
+	static uint32_t state = 0;
+
+	if (seed != 0)
+		state = seed;
+
+	state ^= state << 0xB;
+	state ^= state >> 0x13;
+	state ^= state << 0x7;
+	return state;
+}
+
+inline constexpr uint8_t sbox[256] = {
 	0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76, 0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0, 0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15, 0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75, 0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B,
 	0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84, 0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF, 0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8, 0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2, 0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73, 0x60, 0x81, 0x4F, 0xDC,
 	0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB, 0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79, 0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08, 0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A, 0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1,
 	0x1D, 0x9E, 0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF, 0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16
 };
 
-static constexpr uint8_t Rcon[11] = { 0x8D, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36 };
+inline constexpr uint8_t Rcon[11] = { 0x8D, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36 };
 
 #define Nk 4
 #define Nb 4
@@ -57,8 +71,12 @@ static constexpr uint8_t Rcon[11] = { 0x8D, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 
 
 #define PW_SIZE 15
 
+using AesRoundKey = std::array<uint8_t, AES_ROUND_KEY_SIZE>;
+using AesKey      = std::array<uint8_t, AES_KEY_SIZE>;
+using AesIV       = std::array<uint8_t, AES_IV_SIZE>;
+
 // Init the AES RoundKey
-static inline void keyExpansion(uint8_t *pRoundKey, const uint8_t *pKey)
+inline void keyExpansion(uint8_t *pRoundKey, const uint8_t *pKey)
 {
 	uint8_t tempa[4] = { 0 };
 
@@ -74,6 +92,7 @@ static inline void keyExpansion(uint8_t *pRoundKey, const uint8_t *pKey)
 	for (uint32_t i = Nk; i < Nb * (Nr + 1); i++)
 	{
 		uint32_t k = (i - 1) * 4;
+
 		tempa[0]   = pRoundKey[k + 0];
 		tempa[1]   = pRoundKey[k + 1];
 		tempa[2]   = pRoundKey[k + 2];
@@ -87,13 +106,15 @@ static inline void keyExpansion(uint8_t *pRoundKey, const uint8_t *pKey)
 			tempa[2]            = tempa[3];
 			tempa[3]            = u8tmp;
 
+			// This differs between the original and the WolfRPG version
 			tempa[0] = sbox[tempa[0]] ^ Rcon[i / Nk];
 			tempa[1] = sbox[tempa[1]] >> 4;
 			tempa[2] = ~sbox[tempa[2]];
 			tempa[3] = std::rotr(sbox[tempa[3]], 7);
 		}
 
-		uint32_t j = i * 4;
+		const uint32_t j = i * 4;
+
 		k          = (i - Nk) * 4;
 
 		pRoundKey[j + 0] = pRoundKey[k + 0] ^ tempa[0];
@@ -103,19 +124,24 @@ static inline void keyExpansion(uint8_t *pRoundKey, const uint8_t *pKey)
 	}
 }
 
-static inline void addRoundKey(uint8_t *pState, const uint8_t &round, const uint8_t *pRoundKey)
+/////////////////////////////////
+////// AES CTR Crypt -- based on: https://github.com/kokke/tiny-AES-c
+// While this code should be a normal AES CTR implementation, the keyExpansion implementation above is not standard AES
+// it contains minor changes when the tempa values are modified using the sbox values
+
+inline void addRoundKey(uint8_t *pState, const uint8_t &round, const uint8_t *pRoundKey)
 {
 	for (uint32_t i = 0; i < AES_KEY_SIZE; i++)
 		pState[i] ^= pRoundKey[(round * AES_KEY_SIZE) + i];
 }
 
-static inline void subBytes(uint8_t *pState)
+inline void subBytes(uint8_t *pState)
 {
 	for (uint32_t i = 0; i < AES_KEY_SIZE; i++)
 		pState[i] = sbox[pState[i]];
 }
 
-static inline void shiftRows(uint8_t *pState)
+inline void shiftRows(uint8_t *pState)
 {
 	uint8_t temp;
 
@@ -140,12 +166,12 @@ static inline void shiftRows(uint8_t *pState)
 	pState[7]  = temp;
 }
 
-static inline uint8_t xtime(const uint8_t &x)
+inline uint8_t xtime(const uint8_t &x)
 {
 	return ((x << 1) ^ (((x >> 7) & 1) * 0x1b));
 }
 
-static inline void mixColumns(uint8_t *pState)
+inline void mixColumns(uint8_t *pState)
 {
 	uint8_t tmp;
 	uint8_t t;
@@ -165,7 +191,7 @@ static inline void mixColumns(uint8_t *pState)
 }
 
 // AES Cipher
-static inline void cipher(uint8_t *pState, const uint8_t *pRoundKey)
+inline void cipher(uint8_t *pState, const uint8_t *pRoundKey)
 {
 	addRoundKey(pState, 0, pRoundKey);
 
@@ -183,13 +209,13 @@ static inline void cipher(uint8_t *pState, const uint8_t *pRoundKey)
 }
 
 // AES_CTR_xcrypt
-static inline void aesCtrXCrypt(uint8_t *pData, uint8_t *pKey, const uint32_t &size)
+inline void aesCtrXCrypt(uint8_t *pData, uint8_t *pKey, const std::size_t &size)
 {
 	uint8_t state[AES_BLOCKLEN];
 	uint8_t *pIv = pKey + AES_KEY_EXP_SIZE;
 	int32_t bi   = AES_BLOCKLEN;
 
-	for (uint32_t i = 0; i < size; i++, bi++)
+	for (std::size_t i = 0; i < size; i++, bi++)
 	{
 		if (bi == AES_BLOCKLEN)
 		{
@@ -213,6 +239,9 @@ static inline void aesCtrXCrypt(uint8_t *pData, uint8_t *pKey, const uint32_t &s
 		pData[i] ^= state[bi];
 	}
 }
+
+////// AES CTR Crypt
+/////////////////////////////////
 
 struct CryptData
 {
@@ -249,7 +278,7 @@ struct RngData
 	}
 };
 
-static inline uint32_t customRng1(RngData &rd)
+inline uint32_t customRng1(RngData &rd)
 {
 	uint32_t state;
 	uint32_t stateMod;
@@ -286,7 +315,7 @@ static inline uint32_t customRng1(RngData &rd)
 	return state;
 }
 
-static inline uint32_t customRng2(RngData &rd)
+inline uint32_t customRng2(RngData &rd)
 {
 	uint32_t stateMod;
 	uint32_t state;
@@ -318,7 +347,7 @@ static inline uint32_t customRng2(RngData &rd)
 	return state;
 }
 
-static inline uint32_t customRng3(RngData &rd)
+inline uint32_t customRng3(RngData &rd)
 {
 	uint32_t state;
 	uint32_t seed = rd.seed2;
@@ -342,7 +371,7 @@ static inline uint32_t customRng3(RngData &rd)
 	return state;
 }
 
-static inline void rngChain(RngData &rd, std::vector<uint32_t> &data)
+inline void rngChain(RngData &rd, std::vector<uint32_t> &data)
 {
 	uint32_t i = 0;
 	for (uint32_t &d : data)
@@ -388,7 +417,7 @@ static inline void rngChain(RngData &rd, std::vector<uint32_t> &data)
 	}
 }
 
-static inline void runCrypt(RngData &rd, const uint32_t &seed1, const uint32_t &seed2)
+inline void runCrypt(RngData &rd, const uint32_t &seed1, const uint32_t &seed2)
 {
 	rd.seed1   = seed1;
 	rd.seed2   = seed2;
@@ -400,7 +429,7 @@ static inline void runCrypt(RngData &rd, const uint32_t &seed1, const uint32_t &
 		rngChain(rd, rd.data[i]);
 }
 
-static inline void aLotOfRngStuff(RngData &rd, uint32_t a2, uint32_t a3, const uint32_t &idx, std::vector<uint8_t> &cryptData)
+inline void aLotOfRngStuff(RngData &rd, uint32_t a2, uint32_t a3, const uint32_t &idx, std::vector<uint8_t> &cryptData)
 {
 	uint32_t itrs = 20;
 
@@ -449,7 +478,7 @@ static inline void aLotOfRngStuff(RngData &rd, uint32_t a2, uint32_t a3, const u
 	cryptData[idx] += a3;
 }
 
-static inline void aesKeyGen(CryptData &cd, RngData &rd, std::array<uint8_t, AES_KEY_SIZE> &aesKey, std::array<uint8_t, AES_IV_SIZE> &aesIv)
+inline void aesKeyGen(CryptData &cd, RngData &rd, std::array<uint8_t, AES_KEY_SIZE> &aesKey, std::array<uint8_t, AES_IV_SIZE> &aesIv)
 {
 	runCrypt(rd, cd.seedBytes[0], cd.seedBytes[1]);
 
@@ -466,7 +495,7 @@ static inline void aesKeyGen(CryptData &cd, RngData &rd, std::array<uint8_t, AES
 
 	srand(seed);
 
-	for (uint32_t i = 0; i < RngData::DATA_VEC_LEN; ++i)
+	for (uint32_t i = 0; i < RngData::DATA_VEC_LEN; i++)
 	{
 		uint32_t rn = rand();
 		uint8_t old = indexes[i];
@@ -484,16 +513,16 @@ static inline void aesKeyGen(CryptData &cd, RngData &rd, std::array<uint8_t, AES
 	std::copy(ivBegin, ivBegin + AES_IV_SIZE, aesIv.begin());
 }
 
-static inline uint32_t genMTSeed(const std::array<uint8_t, 3> &seeds)
+inline uint32_t genMTSeed(const std::array<uint8_t, 3> &seeds)
 {
-	int32_t seedP1  = (seeds[1] | (seeds[0] << 8)) << 8;
-	uint32_t seedP2 = ((((seeds[2] | seedP1) << 13) ^ (seeds[2] | (uint32_t)seedP1)) >> 17) ^ ((seeds[2] | seedP1) << 13) ^ (seeds[2] | seedP1);
-	uint32_t seed   = (32 * seedP2) ^ seedP2;
+	uint32_t x = (seeds[0] << 16) | (seeds[1] << 8) | seeds[2];
+	uint32_t y = (x << 13) ^ x;
+	uint32_t z = (y >> 17) ^ y;
 
-	return seed;
+	return z ^ (z << 5);
 }
 
-static inline void decrpytProV2P1(std::vector<uint8_t> &data, const uint32_t &seed)
+inline void decrpytProV2P1(std::vector<uint8_t> &data, const uint32_t &seed)
 {
 	const uint32_t NUM_RNDS = 128;
 
@@ -509,7 +538,7 @@ static inline void decrpytProV2P1(std::vector<uint8_t> &data, const uint32_t &se
 		data[i] ^= rnds[i % NUM_RNDS];
 }
 
-static inline void initCryptProt(CryptData &cd)
+inline void initCryptProt(CryptData &cd)
 {
 	uint32_t fileSize = static_cast<uint32_t>(cd.gameDatBytes.size());
 
@@ -531,7 +560,7 @@ static inline void initCryptProt(CryptData &cd)
 	cd.seed2 = cd.keyBytes[1] ^ cd.keyBytes[2];
 }
 
-static inline CryptData decryptV2File(const std::vector<uint8_t> &bytes)
+inline CryptData decryptV2File(const std::vector<uint8_t> &bytes)
 {
 	CryptData cd;
 	RngData rd;
@@ -541,12 +570,12 @@ static inline CryptData decryptV2File(const std::vector<uint8_t> &bytes)
 
 	runCrypt(rd, cd.seed1, cd.seed2);
 
-	std::array<uint8_t, AES_KEY_SIZE> aesKey;
-	std::array<uint8_t, AES_IV_SIZE> aesIv;
+	AesKey aesKey;
+	AesIV aesIv;
 
 	aesKeyGen(cd, rd, aesKey, aesIv);
 
-	std::array<uint8_t, AES_ROUND_KEY_SIZE> roundKey;
+	AesRoundKey roundKey;
 
 	keyExpansion(roundKey.data(), aesKey.data());
 	std::copy(aesIv.begin(), aesIv.end(), roundKey.begin() + AES_KEY_EXP_SIZE);
